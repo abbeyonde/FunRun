75577f8af09918385d60c37fc4957f4e7852a7e5
abbeyonde <musabsalihin242@gmail.com>
2024-01-09 03:00:05 +0800

    fix: validate existing account

--- add-participant.php
+++ add-participant.php
@@ -62,9 +62,9 @@
     <!-- Header -->
     <?php
     include('connect.php');
-    
+
     session_start();
-    
+
     include("component/navbarParticipant.php"); //import navbar
     ?>
     <!-- Header -->
@@ -78,60 +78,74 @@
     $email = $_POST['inputemail'];
     $address = $_POST['inputaddress'];
 
-    $sql = "INSERT INTO participants VALUES ('','$name','$ic','$password','$age','$email','$address','$phone')";
-    $result = mysqli_query($con, $sql);
-    if ($result == null) { ?>
-        <div class="col-9 mx-auto">
-            <h1>Fail to register participant's profile</h1>
-            <p>Please try again. Click <a href="index.php">here</a></p>
-        </div>
-        <!-- Start Footer -->
-        <?php include("component/footer.php"); ?>
-        <!-- End Footer -->
+    $sql_check = "SELECT * FROM participants WHERE ic=$ic";
+    $result_check = mysqli_query($con, $sql_check);
+    $row_check = mysqli_fetch_array($result_check);
+    if ($row_check == null) {
 
-        <!-- Bootstrap -->
-        <script src="assets/js/bootstrap.bundle.min.js"></script>
-        <!-- Load jQuery require for isotope -->
-        <script src="assets/js/jquery.min.js"></script>
-        <!-- Isotope -->
-        <script src="assets/js/isotope.pkgd.js"></script>
-        <!-- Page Script -->
-        <script>
-            $(window).load(function () {
-                // init Isotope
-                var $projects = $('.projects').isotope({
-                    itemSelector: '.project',
-                    layoutMode: 'fitRows'
-                });
-                $(".filter-btn").click(function () {
-                    var data_filter = $(this).attr("data-filter");
-                    $projects.isotope({
-                        filter: data_filter
+        $sql = "INSERT INTO participants VALUES ('','$name','$ic','$password','$age','$email','$address','$phone')";
+        $result = mysqli_query($con, $sql);
+        if ($result == null) { ?>
+            <div class="col-9 mx-auto">
+                <h1>Fail to register participant's profile</h1>
+                <p>Please try again. Click <a href="index.php">here</a></p>
+            </div>
+            <!-- Start Footer -->
+            <?php include("component/footer.php"); ?>
+            <!-- End Footer -->
+
+            <!-- Bootstrap -->
+            <script src="assets/js/bootstrap.bundle.min.js"></script>
+            <!-- Load jQuery require for isotope -->
+            <script src="assets/js/jquery.min.js"></script>
+            <!-- Isotope -->
+            <script src="assets/js/isotope.pkgd.js"></script>
+            <!-- Page Script -->
+            <script>
+                $(window).load(function () {
+                    // init Isotope
+                    var $projects = $('.projects').isotope({
+                        itemSelector: '.project',
+                        layoutMode: 'fitRows'
                     });
-                    $(".filter-btn").removeClass("active");
-                    $(".filter-btn").removeClass("shadow");
-                    $(this).addClass("active");
-                    $(this).addClass("shadow");
-                    return false;
+                    $(".filter-btn").click(function () {
+                        var data_filter = $(this).attr("data-filter");
+                        $projects.isotope({
+                            filter: data_filter
+                        });
+                        $(".filter-btn").removeClass("active");
+                        $(".filter-btn").removeClass("shadow");
+                        $(this).addClass("active");
+                        $(this).addClass("shadow");
+                        return false;
+                    });
                 });
-            });
 
 
+            </script>
+            <!-- Templatemo -->
+            <script src="assets/js/templatemo.js"></script>
+            <!-- Custom -->
+            <script src="assets/js/custom.js"></script>
+            <script src="assets/js/timer.js"></script>
+
+        </body>
+
+        </html>
+        <?php
+        } else { ?>
+        <script>
+            window.location.href = "signin.php";
         </script>
-        <!-- Templatemo -->
-        <script src="assets/js/templatemo.js"></script>
-        <!-- Custom -->
-        <script src="assets/js/custom.js"></script>
-        <script src="assets/js/timer.js"></script>
-
-    </body>
-
-    </html>
-    <?php
-    } else { ?>
-    <script>
-        window.location.href = "signin.php";
-    </script>
+        <?php
+        }
+    } else {
+        // include("component/navbarParticipant.php");
+        ?>
+    <div class="col-12 mt-5 text-center">
+        <h1>Existing account detected</h1>
+        <p>Go back <a href="index.php">Home</a></p>
+    </div>
     <?php
     }
     ?>
